afe16db05b7303dc5eca441a76c99af9733c7716
0xBriz <chaindevone@gmail.com>
2022-10-20 12:29:36 -0400

    deployment

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,17 +1,12 @@
 import { ethers } from "hardhat";
 
-async function deployFactory() {
-  const SmartChefFactory = await ethers.getContractFactory("SmartChefFactory");
-  const factory = await SmartChefFactory.deploy();
+async function main() {
+  const vault = "0xEE1c8DbfBf958484c6a4571F5FB7b99B74A54AA7";
+  const StablePhantomPoolFactory = await ethers.getContractFactory("StablePhantomPoolFactory");
+  const factory = await StablePhantomPoolFactory.deploy(vault);
   await factory.deployed();
 
-  console.log(`SmartChefFactory deployed to ${factory.address}`);
-}
-
-async function deployPool() {}
-
-async function main() {
-  await deployFactory();
+  console.log(`StablePhantomPoolFactory deployed to ${factory.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
